2794985ef9376f7a8de327697bcd5bd747760875
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-09-20 19:25:46 +0300

    add new menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 //mobile nav
-const btn = document.querySelector('.mobile-menu-button');
-const sidebar = document.querySelector('.sidebar');
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
 
-btn.addEventListener('click', () => {
-  sidebar.classList.toggle('-translate-x-full');
+const navOpenedClass = 'left-[10px]';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-menu-unfold-fill';
+const arrowRightClass = 'ri-menu-fold-fill';
+
+closeBtn.addEventListener('click', () => {
+  if (mobileNav.classList.contains(navClosedClass)) {
+    mobileNav.classList.toggle(navOpenedClass);
+
+    closeBtnIcon.classList.toggle(arrowLeftClass);
+    closeBtnIcon.classList.toggle(arrowRightClass);
+  }
 });
